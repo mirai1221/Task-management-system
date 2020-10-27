ed3af359f8c74f7231c934b4fd889bdb281503d4
藤原未来 <www.motimoti12@gmail.com>
2020-10-27 22:56:14 +0900

    rootをindexに指定　resourcesメソッドを使用して各アクションのルーティングの設定を行う

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index'
+  resources :tasks #resourcesメソッドは全てのアクションに関するルーティングを一括で設定してくれる
+  
 end
