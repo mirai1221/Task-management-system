f605425c665b81e553782db1efc4071f8ca939bd
藤原未来 <www.motimoti12@gmail.com>
2020-10-28 00:50:09 +0900

    newアクションから送られてきたデータをtask_paramsメソッドで取得しcreateアクションでデータベースに保存する

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,6 +9,17 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    task = Task.new(task_params)#安全なデータを使ってオブジェクトを作成しデータベースに保存する
+    task.save!
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+  end
+
   def edit
+  end
+
+  private
+  def task_params
+    params.require(:task).premit(:name,:description)
   end
 end
