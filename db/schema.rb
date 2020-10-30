5146880ea0a67c94d7e949422cb24d66b47a0f3e
藤原未来 <www.motimoti12@gmail.com>
2020-10-30 16:57:09 +0900

    エラーが出る様に設定

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2020_10_27_134338) do
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
