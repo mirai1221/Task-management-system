a38de7ee01e25214e3be7f005abe22d03390f45a
藤原未来 <www.motimoti12@gmail.com>
2020-11-05 17:50:34 +0900

    管理者のrouteingの設定 urlに/adminがつく様にヘルパーメソッドにはadmin_がつくように設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do #urlに/admin、ヘルパーメソッドにadmin_がつくようになっている
+    resources :users
+  end
   root to: 'tasks#index'
-  resources :tasks #resourcesメソッドは全てのアクションに関するルーティングを一括で設定してくれる
-  
+  resources :tasks #resourcesメソッドは全てのアクションに関するルーティングを一括で設定してくれ
 end
