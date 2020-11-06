3754aa4be86480985f27e0986bba31d653dea7fd
藤原未来 <www.motimoti12@gmail.com>
2020-11-06 18:03:05 +0900

    ログイン、ログアウトの設定

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,25 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: session_params[:email])
+
+    if user&.authenticate(session_params[:password])#Userクラスにhas_secure_passwordと記述していると自動で追加されるメソッド　引数で受け取った値をハッシュ化して、その結果がUserオブジェクトのdigestと一致するか調べる
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'ログインしました。'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_path, notice: 'ログアウトしました。'
+  end
+
+  private
+  def session_params
+    params.require(:session).permit(:email,:password)
+  end
 end
