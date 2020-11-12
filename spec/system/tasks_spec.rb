83e4510a0ad0e8d1b4f005aff5a3dfac0f94a430
藤原未来 <www.motimoti12@gmail.com>
2020-11-12 23:31:16 +0900

    itの共通化

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -4,7 +4,7 @@ describe 'タスク管理機能', type: :system do
   let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') } # ユーザーAを作成しuser_aに入れる :userでデータ内容がusers.rbのデータ内容になっている
   let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
   let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', user: user_a) } # let!によって呼び出されなくても定義される 作成者がuser_aであるタスクを作成する
-  
+
   before do
     #ログイン部分の共通化
     visit login_path
@@ -13,12 +13,14 @@ describe 'タスク管理機能', type: :system do
     click_button 'ログイン'
   end
 
+  shared_examples_for 'ユーザーAが作成したタスクが表示される' do #itの共通化
+    it { expect(page).to have_content '最初のタスク' } # 作成済みのタスクの名称が画面上に表示されていることを確認
+  end
+
   describe '一覧表示機能' do
     context 'ユーザーAがログインしているとき' do
       let(:login_user) {user_a} #ログイン共通部分で使われる為に定義している
-      it 'ユーザーAが作成したタスクが表示される' do
-        expect(page).to have_content '最初のタスク' # 作成済みのタスクの名称が画面上に表示されていることを確認
-      end
+      it_behaves_like 'ユーザーAが作成したタスクが表示される' # shared_examples_forを呼び出している
     end
 
     context 'ユーザBがログインしているとき' do
@@ -30,7 +32,7 @@ describe 'タスク管理機能', type: :system do
     end
   end
 
-  describe '詳細表示' do
+  describe '詳細表示機能' do
     context 'ユーザーAがログインしているとき' do
       let(:login_user) {user_a}
 
@@ -38,8 +40,34 @@ describe 'タスク管理機能', type: :system do
         visit task_path(task_a)
       end
 
-      it 'ユーザAが作成したタスクが表示される' do
-        expect(page).to have_content '最初のタスク' # 作成済みのタスクの名称が画面上に表示されていることを確認
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
+    end
+  end
+
+  describe '新規作成機能' do
+    let(:login_user) {user_a}
+
+    before do
+      visit new_task_path
+      fill_in '名称', with: task_name # contextのlet(:task_nameを引用している)
+      click_button '登録する'
+    end
+
+    context '新規作成画面で名称を入力したとき' do
+      let(:task_name) { '新規作成のテストを書く' }
+
+      it '正常に登録される' do
+        expect(page).to have_selector '.alert-success', text: '新規作成のテストを書く'
+      end
+    end
+
+    context '新規作成画面で名称を入力しなかったとき' do
+      let(:task_name) { '' }
+
+      it 'エラーとなる' do
+        within '#error_explanation' do
+          expect(page).to have_content '名称を入力してください'
+        end
       end
     end
   end
