17b77e2f129b29404ef17ea15c7ea00645af54d4
藤原未来 <www.motimoti12@gmail.com>
2020-11-26 14:30:24 +0900

    テストユーザーの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,10 @@ User.find_or_create_by!(email: 'admin@example.com') do |user|
   user.password = 'password'
   user.password_confirmation = 'password'
 end
+
+User.find_or_create_by!(email: 'test@example.com') do |user|
+  user.name: = 'test'
+  user.admin = false
+  user.password = 'password'
+  user.password_confirmation = 'password'
+end
