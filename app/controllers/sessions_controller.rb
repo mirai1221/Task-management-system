7efa0140f7b42c61cbc43bfb1b25541e034539e7
藤原未来 <www.motimoti12@gmail.com>
2021-01-06 23:15:57 +0900

    リファクタリング

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: session_params[:email])
-
-    if user&.authenticate(session_params[:password]) # Userクラスにhas_secure_passwordと記述していると自動で追加されるメソッド　引数で受け取った値をハッシュ化して、その結果がUserオブジェクトのdigestと一致するか調べる
+    # Userクラスにhas_secure_passwordと記述していると自動で追加されるメソッド 引数で受け取った値をハッシュ化して、その結果がUserオブジェクトのdigestと一致するか調べる
+    if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
       redirect_to root_path, notice: 'ログインしました。'
     else
--- spec/mailers/task_mailer_spec.rb
+++ spec/mailers/task_mailer_spec.rb
@@ -1,35 +1,37 @@
-require 'rails_helper'
+# メール機能停止
 
-describe TaskMailer, type: :mailer do
-  let(:task) { FactoryBot.create(:task, name: 'メイラーSpecを書く', description: '送信したメールの内容を確認します') }
+# require 'rails_helper'
 
-  let(:text_body) do
-    part = mail.body.parts.detect { |part| part.content_type == 'text/plain; charset=UTF-8' }
-    part.body.raw_source
-  end
-  let(:html_body) do
-    part = mail.body.parts.detect { |part| part.content_type == 'text/html; charset=UTF-8' }
-    part.body.raw_source
-  end
+# describe TaskMailer, type: :mailer do
+#   let(:task) { FactoryBot.create(:task, name: 'メイラーSpecを書く', description: '送信したメールの内容を確認します') }
 
-  describe '#creation_email' do
-    let(:mail) { TaskMailer.creation_email(task) }
+#   let(:text_body) do
+#     part = mail.body.parts.detect { |part| part.content_type == 'text/plain; charset=UTF-8' }
+#     part.body.raw_source
+#   end
+#   let(:html_body) do
+#     part = mail.body.parts.detect { |part| part.content_type == 'text/html; charset=UTF-8' }
+#     part.body.raw_source
+#   end
 
-    it '想定どおりのメールが生成されている' do
-      # ヘッダ
-      expect(mail.subject).to eq('タスク作成完了メール')
-      expect(mail.to).to eq(['user@example.com'])
-      expect(mail.from).to eq(['taskleaf@example.com'])
+#   describe '#creation_email' do
+#     let(:mail) { TaskMailer.creation_email(task) }
 
-      # text形式の本文
-      expect(text_body).to match('以下のタスクを作成しました')
-      expect(text_body).to match('メイラーSpecを書く')
-      expect(text_body).to match('送信したメールの内容を確認します')
+#     it '想定どおりのメールが生成されている' do
+#       # ヘッダ
+#       expect(mail.subject).to eq('タスク作成完了メール')
+#       expect(mail.to).to eq(['user@example.com'])
+#       expect(mail.from).to eq(['taskleaf@example.com'])
 
-      # html形式の本文
-      expect(html_body).to match('以下のタスクを作成しました')
-      expect(html_body).to match('メイラーSpecを書く')
-      expect(html_body).to match('送信したメールの内容を確認します')
-    end
-  end
-end
+#       # text形式の本文
+#       expect(text_body).to match('以下のタスクを作成しました')
+#       expect(text_body).to match('メイラーSpecを書く')
+#       expect(text_body).to match('送信したメールの内容を確認します')
+
+#       # html形式の本文
+#       expect(html_body).to match('以下のタスクを作成しました')
+#       expect(html_body).to match('メイラーSpecを書く')
+#       expect(html_body).to match('送信したメールの内容を確認します')
+#     end
+#   end
+# end
